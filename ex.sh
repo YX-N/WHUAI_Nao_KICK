34e449a42345bb3e60703cc855aca8551ada06ce
ZZH <1904770276@qq.com>
2025-03-29 21:07:43 +0800

    标定文件

--- ex.sh
+++ ex.sh
@@ -2,107 +2,80 @@
 
 
 echo "press number key to choice"
-echo "-------------1.save copy------------"
-echo "--------2.load calibration---------"
-echo "--------3.download calibration---------"
-echo "----------4.save default-----------"
-echo "---------5.change default----------"
-echo "标定完成后，3获取机器人标定信息，1备份标定信息，2备份的标定信息上传"
+echo "-------------1.load calibration------------"
+echo "-------------2.save calibration------------"
+echo "-------------3.download calibration--------"
 
-read number
+echo "after calibration，1 2 3"
 
-if [ $number -eq 1 ];then
-echo "input field number"
-read num1
-mkdir -p calibration/$num1
-cp -r Config/Robots/ calibration/$num1
+declare -A number_to_word=(
+    [0]="zero"
+    [1]="one"
+    [2]="two"
+    [3]="three"
+    [4]="four"
+    [5]="five"
+    [6]="six"
+    [7]="seven"
+    [8]="eight"
+    [9]="nine"
+    [10]="ten"
+    [11]="eleven"
+    [12]="twelve"
+    [13]="thirteen"
+    [14]="fourteen"
+    [15]="fifteen"
+    [16]="sixteen"
+    [17]="seventeen"
+    [18]="eighteen"
+    [19]="nineteen"
+    [20]="twenty"
+)
 
+read -p "Enter a number: " number
+if [ $number -eq 1 ]; then
+read -p "Enter robot number: " robotnumber
+robotnumber=$((robotnumber + 100))
+./Make/Common/downloadCalibration 10.0.64.$robotnumber
+            
 elif [ $number -eq 2 ];then
-echo "input field number"
-read num1
-rm -rf Config/Robots
-cp -rf calibration/$num1/Robots Config/
-
-elif [ $number -eq 3 ];then
-echo "input robot number"
-read num
-echo "the password you need is 'nao'(only these three English letters)"
-if [ $num -eq 1 ];then
-scp -r nao@192.168.64.101:Config/Robots/one/Body/ ./Config/Robots/one
-scp -r nao@192.168.64.101:Config/Robots/one/one/ ./Config/Robots/one
-
-elif [ $num -eq 2 ];then
-scp -r nao@192.168.5.102:Config/Robots/two/Body/ ./Config/Robots/two
-scp -r nao@192.168.5.102:Config/Robots/two/two/ ./Config/Robots/two
-
-elif [ $num -eq 3 ];then
-scp -r nao@192.168.64.103:Config/Robots/three/Body/ ./Config/Robots/three
-scp -r nao@192.168.64.103:Config/Robots/three/three/ ./Config/Robots/three
-
-elif [ $num -eq 4 ];then
-scp -r nao@192.168.64.104:Config/Robots/four/Body/ ./Config/Robots/four
-scp -r nao@192.168.64.104:Config/Robots/four/four/ ./Config/Robots/four
-
-elif [ $num -eq 5 ];then
-scp -r nao@192.168.5.105:Config/Robots/five/Body/ ./Config/Robots/five
-scp -r nao@192.168.5.105:Config/Robots/five/five/ ./Config/Robots/five
-
-elif [ $num -eq 6 ];then
-scp -r nao@192.168.64.106:Config/Robots/six/Body/ ./Config/Robots/six
-scp -r nao@192.168.64.106:Config/Robots/six/six/ ./Config/Robots/six
-
-elif [ $num -eq 7 ];then
-scp -r nao@192.168.64.107:Config/Robots/seven/Body/ ./Config/Robots/seven
-scp -r nao@192.168.64.107:Config/Robots/seven/seven/ ./Config/Robots/seven
-
-elif [ $num -eq 8 ];then
-scp -r nao@192.168.64.108:Config/Robots/eight/Body/ ./Config/Robots/eight
-scp -r nao@192.168.64.108:Config/Robots/eight/eight/ ./Config/Robots/eight
-
-elif [ $num -eq 9 ];then
-scp -r nao@192.168.64.109:Config/Robots/nine/Body/ ./Config/Robots/nine
-scp -r nao@192.168.64.109:Config/Robots/nine/nine/ ./Config/Robots/nine
-
-elif [ $num -eq 10 ];then
-scp -r nao@192.168.5.110:Config/Robots/ten/Body/ ./Config/Robots/ten
-scp -r nao@192.168.5.110:Config/Robots/ten/ten/ ./Config/Robots/ten
-
-elif [ $num -eq 11 ];then
-scp -r nao@192.168.64.111:Config/Robots/eleven/Body/ ./Config/Robots/eleven
-scp -r nao@192.168.64.111:Config/Robots/eleven/eleven/ ./Config/Robots/eleven
-
-elif [ $num -eq 12 ];then
-scp -r nao@192.168.64.112:Config/Robots/twelve/Body/ ./Config/Robots/twelve
-scp -r nao@192.168.64.112:Config/Robots/twelve/twelve/ ./Config/Robots/twelve
-
-elif [ $num -eq 13 ];then
-scp -r nao@192.168.64.113:Config/Robots/thirteen/Body/ ./Config/Robots/thirteen
-scp -r nao@192.168.64.113:Config/Robots/thirteen/thirteen/ ./Config/Robots/thirteen
-
-elif [ $num -eq 14 ];then
-scp -r nao@192.168.64.114:Config/Robots/fourteen/Body/ ./Config/Robots/fourteen
-scp -r nao@192.168.64.114:Config/Robots/fourteen/fourteen/ ./Config/Robots/fourteen
-
-elif [ $num -eq 15 ];then
-scp -r nao@192.168.64.115:Config/Robots/fifteen/Body/ ./Config/Robots/fifteen
-scp -r nao@192.168.64.115:Config/Robots/fifteen/fifteen/ ./Config/Robots/fifteen
-
-elif [ $num -eq 16 ];then
-scp -r nao@192.168.64.116:Config/Robots/sixteen/Body/ ./Config/Robots/sixteen
-scp -r nao@192.168.64.116:Config/Robots/sixteen/sixteen/ ./Config/Robots/sixteen
+read -p "Enter robot number(0-20): " robotnumber
+read -p "Enter field number(A/B): " fieldnumber
+robotnumber_word=${number_to_word[$robotnumber]}
+source_dir="./Config/Robots/$robotnumber_word"
+target_dir="./calibration/$fieldnumber"
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory $source_dir does not exist."
+    exit 1
 fi
+if [ ! -d "$target_dir" ]; then
+    echo "Target directory $target_dir does not exist. Creating it..."
+    mkdir -p "$target_dir"
+fi
+cp -r "$source_dir" "$target_dir"
+echo "Directory $source_dir has been copied to $target_dir"
 
-elif [ $number -eq 4 ];then
-echo "input robot number"
-read num2
-mkdir -p all_defaults/$num2
-cp -r Config/Robots/Default all_defaults/$num2/
-
-elif [ $number -eq 5 ];then
-echo "input robot number"
-read num2
-rm -rf Config/Robots/Default
-cp -rf all_defaults/$num2/Default Config/Robots
+elif [ $number -eq 3 ]; then
+read -p "Enter field number(A/B): " fieldnumber
+source_dir="./calibration/$fieldnumber"
+target_dir="./Config/Robots"
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory $source_dir does not exist."
+    exit 1
+fi
+if [ ! -d "$target_dir" ]; then
+    echo "Target directory $target_dir does not exist. Creating it..."
+    mkdir -p "$target_dir"
+fi
+for subdir in "$source_dir"/*/; do
+    if [ -d "$subdir" ]; then
+        subdir_name=$(basename "$subdir")
+        target_subdir="$target_dir/$subdir_name"
+        cp -r "$subdir" "$target_dir"
+        echo "Copied $subdir to $target_dir"
+    fi
+done
+echo "All subdirectories from $source_dir have been copied to $target_dir"
 
 fi
 
